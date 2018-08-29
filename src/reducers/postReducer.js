8c774e2bdc1040bedbfdc32d907c704467fd1a86
abc235896 <bc140401486@vu.edu.pk>
2018-08-29 14:50:03 +0500

    action and post reducer added

---
+++ src/reducers/postReducer.js
@@ -0,0 +1,12 @@
+import {FETCH_POSTS, NEW_POST} from "../actoins/type";
+
+const initialState ={
+    items:[],
+    item:{}
+};
+
+export default function (state = initalState ,action) {
+    switch ( action.type ){
+
+    }
+}
